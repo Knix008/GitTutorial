fabf9b9d8d1394157ce0173945484d4c1d39dd66
Suho Kwon <suho.kwon@gmail.com>
2022-06-20 11:54:14 +0900

    Remove some defects

--- src/string_operations.c
+++ src/string_operations.c
@@ -27,7 +27,7 @@ int main(void)
   char p1[32];
   char *s[] = {"Git tutorials", "Tutorials Point"};
   for (i = 0; i < 2; ++i)
-    printf("string lenght of %s = %d\n", s[i], my_strlen(s[i]));
+    printf("string lenght of %s = %ld\n", s[i], my_strlen(s[i]));
   printf("%s\n", my_strcpy(p1, "Hello, World !!!"));
   return 0;
 }
